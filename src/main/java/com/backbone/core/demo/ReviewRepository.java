cd9aa9a23d481611887ab453c6a89c5852f1321f
tansudasli <tansudasli@gmail.com>
2020-05-10 14:57:28 +0300

    comments updated

--- src/main/java/com/backbone/core/demo/ReviewRepository.java
+++ src/main/java/com/backbone/core/demo/ReviewRepository.java
@@ -10,13 +10,37 @@ import java.util.Optional;
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
     Optional<Review> findById(int id);
+
     Optional<List<Review>> findByProductId(Integer productId);
     Optional<List<Review>> findByUserName(String userName);
 
+    /**
+     * if you need paging and sorting, in custom methods, and
+     * call w/ PageRequest(page, size, Sort)
+     *
+     * Optional<List<Product>> findByCategory(String category, Pageable pageable);
+     */
 
-//    for custom queries,if you need
-//    @Query("select p from Product p")
-//    List<Product> retrieveAllProducts();
+    /**
+     * JPQL queries, if you need
+     * - works on JavaObjects,
+     * - suitable for basic CRUD statements
+     * - not suitable for DDL statements
+     *
+     * @Query("select p from Product p where id=:id")
+     * List<Product> retrieveAllProducts(@Param("id" int id));
+     */
+
+    /**
+     * Native (SQL) queries, if you need
+     * - works on Db Tables,
+     * - suitable for complex CRUD statements
+     * - suitable for DDL statements
+     *
+     * @Query(value = "select * from Product p where id=:id", nativeQuery=true)
+     * List<Product> retrieveAllProducts(@Param("id" int id));
+     *
+     */
 
 //    This class has some default methods already implemented such as save(), findAll()
 }
