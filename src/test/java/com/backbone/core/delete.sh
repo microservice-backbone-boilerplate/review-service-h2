213d9feaac8fb194c4db8040079a432d806e4062
tansudasli <tansudasli@gmail.com>
2020-12-12 21:14:51 +0300

    Test scripts are merged

--- src/test/java/com/backbone/core/delete.sh
+++
@@ -1,5 +0,0 @@
-# delete review
-
-curl -H 'Content-Type: application/json' -X DELETE http://localhost:8084/review/100
-
-curl localhost:8084/review/100
--- src/test/java/com/backbone/core/test.sh
+++ src/test/java/com/backbone/core/test.sh
@@ -1,5 +1,22 @@
-# update.create product
+# read requests
 
+echo "get hostname (meaningful in a server)"
+curl localhost:8084/dummy
+
+echo ""
+curl localhost:8084/dummy/Tansu
+
+#read
+printf "\n review/1 >>"
+curl localhost:8084/review/1
+
+printf "\n review/101 : >>"
+curl localhost:8084/review/101
+
+curl localhost:8084/reviews
+curl localhost:8084/reviews/page/0/size/1
+
+#save
 curl localhost:8084/review/10
 
 curl -d '{"id":10,
@@ -42,3 +59,8 @@ curl -H 'Content-Type: application/json' \
 
 
 curl localhost:8084/review/10
+
+#delete
+curl -H 'Content-Type: application/json' -X DELETE http://localhost:8084/review/100
+
+curl localhost:8084/review/100
